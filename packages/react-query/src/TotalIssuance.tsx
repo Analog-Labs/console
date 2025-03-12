8967142291e5c0cfb8bb34506689d5676a40ef46
webdevlapani <webdev.lapani88@gmail.com>
2025-03-12 16:47:02 +0530

    Fix: Total Issuance Calculation by Including Airdrop Amount

--- packages/react-query/src/TotalIssuance.tsx
+++ packages/react-query/src/TotalIssuance.tsx
@@ -1,9 +1,10 @@
 // Copyright 2017-2025 @polkadot/react-query authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { useApi, useCall } from '@polkadot/react-hooks';
+import { BN } from '@polkadot/util';
 
 import FormatBalance from './FormatBalance.js';
 
@@ -15,7 +16,17 @@ interface Props {
 
 function TotalIssuance ({ children, className = '', label }: Props): React.ReactElement<Props> | null {
   const { api } = useApi();
-  const totalIssuance = useCall<string>(api.query.balances?.totalIssuance);
+  const totalIssuanceInitial = useCall<string>(api.query.balances?.totalIssuance);
+  const airdropTotal = useCall<string>(api.query.airdrop.total);
+
+  const totalIssuance = useMemo(
+    () => {
+      const total = new BN(totalIssuanceInitial?.toString() || 0).add(new BN(airdropTotal?.toString() || 0));
+
+      return total;
+    },
+    [airdropTotal, totalIssuanceInitial]
+  );
 
   return (
     <div className={className}>
